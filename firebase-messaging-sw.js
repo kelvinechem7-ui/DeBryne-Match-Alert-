955df3fa44ac5dd9fe66e5eccf123236040d2d53
kelvinechem7-ui <kelvinechem7@gmail.com>
2026-08-09 14:01:51 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,69 +6,36 @@ importScripts(
   "https://www.gstatic.com/firebasejs/12.17.1/firebase-messaging-compat.js"
 );
 
-
 const firebaseConfig = {
-
-  apiKey:
-    "AIzaSyDiv2PjGsMCOpmTqBKzbydWUsFOP36fFtM",
-
-  authDomain:
-    "debryne-match-alert-a2d51.firebaseapp.com",
-
-  databaseURL:
-    "https://debryne-match-alert-a2d51-default-rtdb.firebaseio.com",
-
-  projectId:
-    "debryne-match-alert-a2d51",
-
-  storageBucket:
-    "debryne-match-alert-a2d51.firebasestorage.app",
-
-  messagingSenderId:
-    "967179671646",
-
-  appId:
-    "1:967179671646:web:0cbf28b0f57a146ca2bccf",
-
-  measurementId:
-    "G-R2TYPHXC37"
-
+  apiKey: "AIzaSyDiv2PjGsMCOpmTqBKzbydWUsFOP36fFtM",
+  authDomain: "debryne-match-alert-a2d51.firebaseapp.com",
+  databaseURL: "https://debryne-match-alert-a2d51-default-rtdb.firebaseio.com",
+  projectId: "debryne-match-alert-a2d51",
+  storageBucket: "debryne-match-alert-a2d51.firebasestorage.app",
+  messagingSenderId: "967179671646",
+  appId: "1:967179671646:web:0cbf28b0f57a146ca2bccf"
 };
-
 
 firebase.initializeApp(firebaseConfig);
 
+const messaging = firebase.messaging();
 
-const messaging =
-  firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
 
+  console.log("Background message:", payload);
 
-messaging.onBackgroundMessage(
-  function(payload) {
+  const title =
+    payload.notification?.title ||
+    "DeBryne Match Alert";
 
-    console.log(
-      "Background message received:",
-      payload
-    );
+  const body =
+    payload.notification?.body ||
+    "New match update!";
 
+  self.registration.showNotification(title, {
+    body: body,
+    icon: "/DeBryne-Match-Alert-/icon.png",
+    badge: "/DeBryne-Match-Alert-/icon.png"
+  });
 
-    const title =
-      payload.notification?.title ||
-      "DeBryne Match Alert";
-
-
-    const body =
-      payload.notification?.body ||
-      "New match update!";
-
-
-    self.registration.showNotification(
-      title,
-      {
-        body: body,
-        icon: "/DeBryne-Match-Alert-/icon.png"
-      }
-    );
-
-  }
-);
+});
