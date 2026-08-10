1b9f4c34ba4829692e9712728e9803f17a5ab5e9
kelvinechem7-ui <kelvinechem7@gmail.com>
2026-08-10 23:29:48 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,48 +7,63 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBKHJEeLkkTRc_X6E_ZMaBqGyGkwO87-zo",
-  authDomain: "debryne-update.firebaseapp.com",
-  databaseURL: "https://debryne-update-default-rtdb.firebaseio.com",
-  projectId: "debryne-update",
-  storageBucket: "debryne-update.firebasestorage.app",
-  messagingSenderId: "218054789442",
-  appId: "1:218054789442:web:f891b9a9cd22e7938260db",
-  measurementId: "G-1LNK265F45"
-});
 
-const messaging = firebase.messaging();
+  apiKey:
+    "AIzaSyBKHJEeLkkTRc_X6E_ZMaBqGyGkwO87-zo",
 
-messaging.onBackgroundMessage(function(payload) {
+  authDomain:
+    "debryne-update.firebaseapp.com",
 
-  console.log(
-    "Background notification received:",
-    payload
-  );
+  databaseURL:
+    "https://debryne-update-default-rtdb.firebaseio.com",
 
-  const notificationTitle =
-    payload.notification?.title ||
-    "DeBryne Update";
+  projectId:
+    "debryne-update",
 
-  const notificationOptions = {
+  storageBucket:
+    "debryne-update.firebasestorage.app",
 
-    body:
-      payload.notification?.body ||
-      "You have a new update from DeBryne Update.",
+  messagingSenderId:
+    "218054789442",
 
-    icon: "./icon-192.png",
+  appId:
+    "1:218054789442:web:f891b9a9cd22e7938260db",
 
-    badge: "./icon-192.png",
-
-    data: {
-      url: "./index.html"
-    }
-
-  };
-
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
+  measurementId:
+    "G-1LNK265F45"
 
 });
+
+const messaging =
+  firebase.messaging();
+
+messaging.onBackgroundMessage(
+  function(payload) {
+
+    console.log(
+      "Background notification received:",
+      payload
+    );
+
+    const notificationTitle =
+      payload.notification?.title ||
+      "DeBryne Update";
+
+    const notificationOptions = {
+
+      body:
+        payload.notification?.body ||
+        "You have a new update from DeBryne Update.",
+
+      icon:
+        "./file_000000008e5c71f4a9d057362a556762.png"
+
+    };
+
+    self.registration.showNotification(
+      notificationTitle,
+      notificationOptions
+    );
+
+  }
+);
